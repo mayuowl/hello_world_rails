907813ccbb659c8269d4b5ac310e2d585fd8822f
廣田真由 <my-hrt@i.softbank.jp>
2019-06-15 22:35:13 +0900

    add pry_byebug

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_122503) do
+ActiveRecord::Schema.define(version: 2019_06_14_134956) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "acount"
+    t.string "account"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
