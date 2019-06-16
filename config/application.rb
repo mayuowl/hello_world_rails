9aa87a7800c55fd72da98039f1326df37a0c1975
廣田真由 <my-hrt@i.softbank.jp>
2019-06-16 14:15:23 +0900

    be rails g scaffold articles

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,12 @@ module HelloWorldRails
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.generators do |g|
+      g.template_engine false
       g.javascripts false
       g.stylesheets false
       g.helper false
       g.test_framework false
+      config.api_only = true
     end
   end
 end
