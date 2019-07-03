35fa89997918c047ef20e52e8329d811bdac7d92
廣田真由 <my-hrt@i.softbank.jp>
2019-07-03 21:26:18 +0900

    add communitys table and user_communities table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   validates :account, uniqueness: true
 
   has_many :articles
+  has_many :user_communities
+  has_many :communities, through: :user_communities
   has_one :user_detail
+
 end
