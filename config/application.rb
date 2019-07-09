2e2c5254a0847a6e153e8a5e2110b754a3ef6fb6
廣田真由 <my-hrt@i.softbank.jp>
2019-07-09 20:37:30 +0900

    install rspec and setting

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module HelloWorldRails
       g.javascripts false
       g.stylesheets false
       g.helper false
-      g.test_framework false
+      g.test_framework :rspec
       config.api_only = true
       config.middleware.use ActionDispatch::Flash
     end
