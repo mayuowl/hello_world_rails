3952dd22cf1320f47152bcddd67847d5a3e098b3
廣田真由 <my-hrt@i.softbank.jp>
2019-07-23 14:51:26 +0900

    FactoryBotの設定を追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :user do
+    _account = Faker::Internet.username
+
     name { Faker::Name.name }
-    account { Faker::Internet.username }
-    email { Faker::Internet.email }
+    sequence(:account) { |n| "#{n}_#{_account}" }
+    sequence(:email) { |n| Faker::Internet.email("#{n}_#{_account}") }
+
+    trait :with_user_detail do
+      user_detail
+    end
   end
 end
